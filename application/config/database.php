04a98643562acca4f5de19522ad1c738b5f773ac
SuRAp <msulthanraflim@gmail.com>
2022-06-05 11:47:10 +0700

    Update Files

--- application/config/database.php
+++ application/config/database.php
@@ -70,15 +70,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'development';
 $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
+	'hostname' => 'sql113.epizy.com',
+	'username' => 'epiz_31887039',
+	'password' => 'o5BtXVPZUtrcFA',
+	'database' => 'epiz_31887039_rusun_muka_kuning',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => TRUE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['development'] = array(
+	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'ams_application2',
+	'database' => 'rusun_muka_kuning',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
--- application/views/login.php
+++ application/views/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Apartemen Trilogi</title>
+    <title>Rusun Muka Kuning</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="icon" href="<?php echo base_url('assets/template/backend/dist') ?>/img/AdminLTELogo.png" type="image/x-icon">
     <link rel="stylesheet" href="<?php echo base_url('assets/plugins') ?>/fontawesome-free/css/all.min.css">
